1bdc61d228fdb6642a70820afc07e0cf065051ea
juan lozano <147006115+oedos@users.noreply.github.com>
2025-11-21 22:23:12 -0300

    novo

--- cinema02/src/App.js
+++ cinema02/src/App.js
@@ -5,18 +5,21 @@ import About from './About';
 import NotFound from './NotFound';
 import ProfilePage from './components/ProfilePage';
 import ProdutorPage from './components/ProdutorPage'; 
-
+import CadastroUsuario from './components/CadastroUsuario';
+import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/detalhe/:id" element={<MovieDetail />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/perfil" element={<ProfilePage />} />
         <Route path="/produtor" element={<ProdutorPage />} />
+        <Route path="/cadastro" element={<CadastroUsuario />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
