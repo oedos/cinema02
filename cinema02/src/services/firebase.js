440ff82cab76b38ba1084102b2ab314fda83b318
juan lozano <147006115+oedos@users.noreply.github.com>
2025-11-21 22:19:39 -0300

    novo
    add

--- cinema02/src/services/firebase.js
+++ cinema02/src/services/firebase.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// Sua configuração do Firebase
+export const firebaseConfig = {
   apiKey: "AIzaSyAg28Ccc7vj7l06a4PUGDfCo_DvVBfjG-k",
   authDomain: "cineclipse-10c65.firebaseapp.com",
   projectId: "cineclipse-10c65",
-  storageBucket: "cineclipse-10c65.firebasestorage.app",
+  storageBucket: "cineclipse-10c65.appspot.com", // Corrigido: precisa ter 'appspot.com'
   messagingSenderId: "785062891914",
   appId: "1:785062891914:web:2e49f255e726c869553873",
   measurementId: "G-HKYMJ63KRZ"
 };
 
-// Initialize Firebase
+// Inicializa o Firebase App
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Inicializa e exporta o Firestore
+export const db = getFirestore(app);
+
+// Inicializa e exporta o Auth
+export const auth = getAuth(app);
